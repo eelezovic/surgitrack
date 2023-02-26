d6de9afeb179e226cc2fb3ea167b217c3077dcf5
Emir <elitedesire17@gmail.com>
2023-02-25 23:08:18 -0500

    Created a few components filles Js

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h2>This is the title</h2>
+      <p>This is the content</p>
+    </div>
+  );
+}
+
+export default Note;
