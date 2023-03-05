fa7b22f4ca4d8b59e0288edc1b8b5a4f5809123b
Emir <elitedesire17@gmail.com>
2023-03-04 23:50:37 -0500

    created a login component and its functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,23 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./items";
 import contacts from "./home.contents";
+import Login from "./Login";
 
 
+const isLoggedIn = false;
 
-
+function renderConditionally() {
+  if (isLoggedIn === true) {
+    return <h1>Hello</h1>;
+} else{
+   return <Login />
+  }
+}
 
 function App() {
   return ( 
   <div>
+   <div className="container">{renderConditionally()}</div>
     <Header />
     <Note 
       content={contacts[0].content}
