74d7ea68cb8488290ed10e1a5eef5e67f38cedcc
Emir <elitedesire17@gmail.com>
2023-03-21 12:05:26 -0400

    changes to SingleInstrumentComponent

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,16 @@
 import React, { useState } from "react";
-import styles from "./SearchBar.module.css";
-import Table from "./Table";
-import { SingleInstrumentsData } from "./dataStorage/SingleInstrumentsData";
-import Pagination from "./Pagination";
+
 
 function SearchBar() {
   const [query, setQuery] = useState("");
   const keys = ["instrument_name", "instrument_id", "instrument_location"]
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(7);
-
+  
   const search = (data) => {
     return data.filter(
       (item) => 
-      keys.some(key => item[key].toLowerCase().includes(query)) ||
-      keys.some(key => item[key].toUpperCase().includes(query))
+      keys.some(key => item[key].toUpperCase().includes(query.toUpperCase()))
     );
   }
- 
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = SingleInstrumentsData.slice(indexOfFirstPost, indexOfLastPost);
-
-  // To change pages
-const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
 
   return (
     <div className="inputWrapper">
@@ -34,11 +21,7 @@ const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
       className="search"
       onChange={(e) => setQuery(e.target.value)} 
       />
-      <Table data={search(currentPosts)} /> 
-      <Pagination
-      postsPerPage={postsPerPage} 
-      totalPosts={SingleInstrumentsData.length} 
-      paginate={paginate} />
+      
     </div>
   );
 }
