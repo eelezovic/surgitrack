44b748cb2e69d758d32a478c1db2aef319046720
Emir <elitedesire17@gmail.com>
2023-03-31 17:28:12 -0400

    dropdow menu created for Instrument set

---
+++ src/components/Dropdown.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import styles from "./Dropdown.module.css"
+
+
+function Dropdown (){
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className={styles.dropdown}>
+      <div className={styles.dropdownButton}>Select One</div>
+      <div className={styles.dropdownContent}>
+        <div className={styles.dropdownItem}>
+          React
+        </div>
+        <div className={styles.dropdownItem}>
+          Vue
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default Dropdown;
