5bca99445ef23b6b4d3106e823118f1de971723d
Emir <elitedesire17@gmail.com>
2023-03-31 17:29:20 -0400

    Dropdwon menu created

--- src/pages/InstrumentSetComponent.js
+++ src/pages/InstrumentSetComponent.js
@@ -1,31 +1,14 @@
 import React from "react";
-import styles from "./InstrumentSetComponent.module.css";
 
-const InstrumentSetComponent = ({ data }) => {
+import Dropdown from "../components/Dropdown";
+
+const InstrumentSetComponent = () => {
   return (
-    <div className={styles.tableContainer}>
-      <table>
-        <thead>
-          <tr>
-            <th>Set Name</th>
-            <th>Set ID</th>
-            <th>Quantity</th>
-            <th>Location</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((set) => (
-            <tr key={set.set_id}>
-              <td>{set.set_name}</td>
-              <td>{set.set_id}</td>
-              <td>{set.set_quantity}</td>
-              <td>{set.set_location}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+    <div className="container">
 
+      <Dropdown />
+
+    </div>
+  );     
+}
 export default InstrumentSetComponent;
