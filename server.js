590fe336b10feb36ae710ebe9516e14764c5ce1a
Emir <elitedesire17@gmail.com>
2023-07-08 23:11:42 -0400

    Created server.js for express.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.listen(3000, () => {
+  console.log("running on port 3000")
+})
+// Serve static files from the build folder
+app.use(express.static(path.join(__dirname, "build")));
+
+// Fallback route for serving the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
