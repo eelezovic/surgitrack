936e366e4e1c5b9f28d2e7e392b280c9d780116e
eelezovic <elitedesire17@gmail.com>
2023-07-13 23:56:34 -0400

    Created login and register com.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res) => {
+  res.send("Welcome to Surgitrack")
+});
 
 app.listen(8000, () => { console.log("Server started on port 8000")})
