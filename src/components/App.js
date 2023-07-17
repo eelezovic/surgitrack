7c88fb9038f40119cc4b9991383b4d18bc5f58e6
eelezovic <elitedesire17@gmail.com>
2023-07-16 23:49:01 -0400

    Created paths to Login and Register

--- src/components/App.js
+++ src/components/App.js
@@ -3,27 +3,35 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Home from "../pages/Home";
 import About from "../pages/About";
-//import Login from "./login";
-//import Register from "./register";
+import Login from "./Login";
+import Register from "./Register";
 import { Route, Routes } from "react-router-dom";
 import SingleInstrumentsComponent from "../pages/SingleInstrumentsComponent";
 import InstrumentSetComponent from "../pages/InstrumentSetComponent";
 
-
 function App() {
-  return ( 
-  <> 
-    <Navbar />
+  return (
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    <Routes>
-      <Route path="/singleinstrumentscomponent" element={<SingleInstrumentsComponent />} />
-      <Route path="/InstrumentSetComponent" element={<InstrumentSetComponent />} />
-    </Routes>
-    <Footer />
-  </>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/singleinstrumentscomponent"
+          element={<SingleInstrumentsComponent />}
+        />
+        <Route
+          path="/InstrumentSetComponent"
+          element={<InstrumentSetComponent />}
+        />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
