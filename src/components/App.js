7d81a43c49218f3f5be911a5c061980cabfb4367
eelezovic <elitedesire17@gmail.com>
2023-07-29 10:58:04 -0400

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Home from "../pages/Home";
@@ -8,8 +8,19 @@ import Register from "./Register";
 import { Route, Routes } from "react-router-dom";
 import SingleInstrumentsComponent from "../pages/SingleInstrumentsComponent";
 import InstrumentSetComponent from "../pages/InstrumentSetComponent";
+import PrivateRoutes from "./PrivateRoutes";
+import { toBeRequired } from "@testing-library/jest-dom/dist/matchers";
+import { MdTrendingUp } from "react-icons/md";
 
 function App() {
+  const [isSignedIn, setIsSignedIn] = useState(true)
+  const signin = () => {
+    setIsSignedIn(true)
+  }
+  const signout = () => {
+    setIsSignedIn(false)
+  }
+
   return (
     <>
        <Navbar />
@@ -18,16 +29,18 @@ function App() {
         <Route path="/register" element={<Register />} />
       </Routes>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
+          <Route path="/home" element={<PrivateRoutes isSignedIn={isSignedIn}><Home /></PrivateRoutes>} />
+          <Route path="/about" element={<PrivateRoutes isSignedIn={isSignedIn}><About /></PrivateRoutes>} />
+
+       { /*<Route
           path="/singleinstrumentscomponent"
           element={<SingleInstrumentsComponent />}
         />
         <Route
           path="/InstrumentSetComponent"
           element={<InstrumentSetComponent />}
-        />
+        />*/}
+        
       </Routes>
       <Footer />
     </>
